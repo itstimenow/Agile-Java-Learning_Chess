756ea10903a467ce74850677168d200fcc6917f6
itstimenow <mapeiworld@gmail.com>
2013-05-12 23:04:43 +0800

    Complete exercise 2 to 4 of lesson 9

--- src/languageTest/MapTest.java
+++ src/languageTest/MapTest.java
@@ -36,7 +36,48 @@ public class MapTest extends TestCase {
             result.add(entry.getKey() + ":" + entry.getValue());
         return result;
     }
+    
+    public void testNameEquality() {
+        Name eq1 = new Name("Eric");
+        Name eq2 = new Name("Eric");
+        Name eq3 = new Name("Eric");
+        Name neq = new Name("Bruce");
+        
+        // reflexivity
+        assertEquals(eq1, eq1);
+        
+        // symmetry
+        Name nameAPrime1 = new Name("Eric");
+        assertEquals(eq1, eq2);
+        assertEquals(eq2, eq1);
+        
+        // transitivity
+        assertEquals(eq2, eq3);
+        assertEquals(eq1, eq3);
+        
+        // consistency
+        assertEquals(eq1, eq2);
+        
+        // comparison to null
+        assertFalse(eq1.equals(null));
+        assertFalse(neq.equals(null));
+        
+        // unequality
+        assertFalse(eq1.equals(neq));
+    }
+    
+    public void testNameSet() {
+        Set<Name> names = new HashSet<Name>();
+        names.add(new Name("AAA"));
+        names.add(new Name("Foo"));
+        names.add(new Name("BBB"));
+        
+        assertTrue(names.contains(new Name("AAA")));
+        assertTrue(names.contains(new Name("Foo")));
+        assertTrue(names.contains(new Name("BBB")));
+    }
 }
+
 
 class WordCount {
     
@@ -57,5 +98,36 @@ class WordCount {
         for (Map.Entry<String, Integer> entry : countData.entrySet())
             result.add(entry.getKey() + ":" + entry.getValue());
         return result;
+    }
+}
+
+
+class Name {
+    
+    private String name;
+    
+    public Name(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj == this)
+            return true;
+        if (obj.getClass() != this.getClass())
+            return false;
+        
+        Name that = (Name)obj;
+        if (that.name == this.name)
+            return true;
+        
+        return false;
+    }
+    
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
