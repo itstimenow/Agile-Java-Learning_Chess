8959ce308c3ba733e873b602a9ad6fabb366af5d
itstimenow <mapeiworld@gmail.com>
2013-05-17 20:06:13 +0800

    Complete exercise 6 of lesson 10

--- src/languageTest/lesson10/NumericTest.java
+++ src/languageTest/lesson10/NumericTest.java
@@ -12,4 +12,40 @@ public class NumericTest extends TestCase {
         String octalStr = "0" + Integer.toOctalString(n);
         assertTrue(octalStr.equals("0157255"));
     }
+    
+    public void testNaN() {
+        isNaN(Double.NaN);
+        isNaN(0.0 / 0.0);
+        isNaN(0.0 / 0);
+        isNaN(0 / 0.0);
+        isNaN(0 % 0.0);
+        isNaN(Double.NaN + Double.NaN);
+        isNaN(Math.sqrt(-1));
+        isNaN(Double.POSITIVE_INFINITY + Double.NEGATIVE_INFINITY);
+        isNaN(Double.NEGATIVE_INFINITY / Double.POSITIVE_INFINITY);
+        isNaN(Double.POSITIVE_INFINITY % 10);
+    }
+    
+    private void isNaN(double value) {
+        assertTrue(Double.isNaN(value));
+    }
+    
+    public void testInfinite() {
+        isInfinite(1.0 / 0);
+        isNegativeInfinite(Double.NEGATIVE_INFINITY);
+        isNegativeInfinite(Double.NEGATIVE_INFINITY * Double.POSITIVE_INFINITY);
+        isPositiveInfinite(Double.NEGATIVE_INFINITY * Double.NEGATIVE_INFINITY);
+    }
+    
+    private void isInfinite(double value) {
+        assertTrue(Double.isInfinite(value));
+    }
+    
+    private void isPositiveInfinite(double value) {
+        assertTrue(value == Double.POSITIVE_INFINITY);
+    }
+    
+    private void isNegativeInfinite(double value) {
+        assertTrue(value == Double.NEGATIVE_INFINITY);
+    }
 }
